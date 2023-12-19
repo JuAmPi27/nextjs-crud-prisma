f8f990bb6521dbb34d6aa76b7af99e56196acc5e
Juampi27 <jpbarnetche27@gmail.com>
2023-12-19 19:00:58 -0300

    listado de tareas a traves de un componente del servidor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,28 @@
+import { prisma } from "@/libs/prisma";
 
-function HomePage() {
+
+async function loadTasks() {
+  return await prisma.task.findMany();
+}
+
+async function HomePage() {
+  const tasks = await loadTasks();
+
+  // cada elemento que se recorre en react tiene q tener un key unico, en este caso task.id
   return (
-    <div>TAREAS:</div>
+  <section className="container mx-auto">
+    <div className="grid grid-cols-3 gap-3 mt-10">
+    {tasks.map(task => (
+      <div key={task.id} className="bg-slate-900 p-3 rounded hover:bg-slate-800 hover::cursor-pointer">
+        <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
+        <p>{task.description}</p>
+        <hr/>
+        <p>creado el: {new Date(task.createdAt).toLocaleDateString()}</p>
+      </div>
+    ))}
+  </div>
+  </section>
   )
 }
 
-export default HomePage
+export default HomePage;
